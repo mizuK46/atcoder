c777a7bca75bfdea2c2b685f42453ccae6ab14fc
sujiko46 <kyosuke.mizukami@jmty.jp>
2020-04-20 14:45:27 +0900

    ABC087B - Coins

---
+++ q5.rb
@@ -0,0 +1,11 @@
+A = gets.chomp.to_i
+B = gets.chomp.to_i
+C = gets.chomp.to_i
+X = gets.chomp.to_i
+
+result = 0
+(0..A).each do |a|
+  (0..B).each do |b|
+    (0..C).each { |c| result += 1 if a * 500 + b * 100 + c * 50 == X }
+  end
+end
