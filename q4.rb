a3110a38db90beea286fc854a1a8530a1c144aa3
sujiko46 <kyosuke.mizukami@jmty.jp>
2020-04-20 17:00:52 +0900

    リファクタ

--- q4.rb
+++ q4.rb
@@ -2,23 +2,8 @@
 iranai = gets
 a = gets.chomp.split(" ").map(&:to_i)
 
-def check_even_all(list)
-  @flag = true
-  list.map do |i|
-    @flag = i.even?
-    break if @flag == false
-  end
-  @flag
-end
-
-def get_half(list)
-  list.map do |i|
-    i / 2
-  end
-end
-
-while check_even_all(a)
-  a = get_half(a)
+while a.all?(&:even?)
+  a = a.map { |i| i / 2 }
   @count += 1
 end
 p @count
